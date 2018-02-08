9aff061e69a9515e032644e8c4be813b3cedb7f0
id.druzhin <id.druzhin@gmail.com>
2018-02-08 16:58:14 +0300

    Texture index fix

--- VoxelProject/RayCastingPS.hlsl
+++ VoxelProject/RayCastingPS.hlsl
@@ -9,25 +9,41 @@ cbuffer RenderingConstantBuffer : register(b0)
 	float4x4 WorldViewProj;
 };
 
-cbuffer DirectConstants : register(b1)
-{
-	uint textureIndex;
-}
-
+uint textureIndex : register(b1);
 SamplerState s : register(s0);
 RWTexture2D<float4> renderTexture : register(u1);
 RWTexture2D<float4> backCoordTexture : register(u2);
 Texture1D<float4> palette : register(t0);
 Texture1D<float> segmentsOpacity : register(t1);
-Texture3D<uint2> textures[] : register(t3);
+//Texture3D<uint2> textures[] : register(t2);
+Texture3D<float2> textures[] : register(t2);
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	//uint2 index = textures[textureIndex].Sample(s,input.texCoord);
+	//float index = textures[textureIndex][uint3(0,0,0)];
+	//float index = textures[textureIndex].Load(uint4(0,0,0,0));
+
+	uint2 index = textures[textureIndex].Sample(s,input.texCoord);
+
+	//uint2 index = textures[textureIndex].Sample(s,input.texCoord);
+	//uint2 index = textures[textureIndex].SampleLevel(s,input.texCoord,0);
+
+	if (index.y != 0)
+	{
+		renderTexture[input.pos.xy] = palette[index.x];
+	}
+	
+
+	//renderTexture[input.pos.xy] = float4(textureIndex/256.0f,0,0,1);
+	//uint2 index = textures[textureIndex].SampleLevel(s,input.texCoord,0);
+	//uint2 index = textures[textureIndex].Load(input.texCoord);
+	//renderTexture[input.pos.xy] = palette[index.x];
 	//return textures[textureIndex].Sample(s,input.texCoord);
 	//renderTexture[input.pos.xy] = float4(input.texCoord, 1.0f) - backCoordTexture[input.pos.xy];
-	renderTexture[input.pos.xy] = backCoordTexture[input.pos.xy];
+	//renderTexture[input.pos.xy] = backCoordTexture[input.pos.xy];
 	discard;
+	//return palette[index.x];
 	return float4(0.0f,0.0f,0.0f,0.0f);
 	//return float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
